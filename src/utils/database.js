4d9c515fbdbc664e78d79f75da116d0f0b062026
oscarcortesm98 <55117362+oscarcortesm98@users.noreply.github.com>
2023-10-04 22:19:37 +0200

    Add files via upload

--- src/utils/database.js
+++ src/utils/database.js
@@ -8,6 +8,7 @@ const db = new Sequelize({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     dialect: "postgres",
+    dialectOptions: {ssl: {required: true, rejectUnauthorized: false}}
 });
 
 export default db;
